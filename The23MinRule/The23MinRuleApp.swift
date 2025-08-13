e8b1477fbc7d33804604d1a0cb9af260e9435250
karimmufti <karroomdj2005@gmail.com>
2025-08-13 18:30:01 -0500

    Initial Commit

---
+++ The23MinRule/The23MinRuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  The23MinRuleApp.swift
+//  The23MinRule
+//
+//  Created by Karim Mufti on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct The23MinRuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
